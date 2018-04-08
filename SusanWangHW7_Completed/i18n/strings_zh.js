b5b481d7f24118474c495054b1f9492ef6f0b38b
ohsusieq98 <susan.sheng89@Gmail.com>
2018-04-08 14:35:13 -0700

    actual bunnies to be slaughtered

---
+++ SusanWangHW7_Completed/i18n/strings_zh.js
@@ -0,0 +1,20 @@
+var i18n = {
+    "Bunnies splattered": "溅洒的兔子一共有：",
+    "Bunnies eviscerated":"毁掉的兔子一共有",
+    "Bunnies mutilated": "切断的兔子一共有",
+    "Bunnies massacred": "残杀的兔子一共有",
+    "Bunnies butchered": "屠杀的兔子一共有",
+    "reload your shotgun!": "重新装子弹！",  
+    "make the killing stop!": "不要再杀了！",
+    "splattered": "打死的兔子有",
+    "Images loaded: ": "加载图片：",
+    "The bunnies of the world thank you for your pacifism. Happy Easter!": "世界上的兔子对你和平主义态度表示感谢！复活节快乐！", 
+    " Level ": " 轮", 
+    "Bunny Hunt IV": "寻找兔子 4", 
+    "The Good Friday Massacre": "受难日大屠杀", 
+    "let\'s shoot some bunnies!": "咱们去打猎吧！"
+    
+    
+    
+    
+}
---
+++ SusanWangHW7_Completed/js/24ways.js
@@ -0,0 +1,6 @@
+function _(s) {
+    if (typeof(i18n)!='undefined' && i18n[s]) {
+        return i18n[s];
+    }
+    return s;
+}
